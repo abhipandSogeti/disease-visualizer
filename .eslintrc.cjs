92d81711e13a9e8602c869a278d22503c96f779c
Abhishek Pandit <abhipandSogeti@users.noreply.github.com>
2026-05-27 16:14:08 +0200

    fix: use recommended-type-checked and add project parserOptions in eslintrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,7 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
     'plugin:react-hooks/recommended',
     'prettier',
   ],
@@ -12,6 +12,8 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
+    project: ['./tsconfig.json'],
+    tsconfigRootDir: __dirname,
   },
   rules: {
     '@typescript-eslint/no-explicit-any': 'error',
