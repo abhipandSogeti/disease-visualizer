88bcc02952b2c1fd09cf42646c63e20d79a986a3
Abhishek Pandit <abhipandSogeti@users.noreply.github.com>
2026-05-27 16:10:51 +0200

    chore: add husky, commitlint, eslint, prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'vitest.config.ts', 'vite.config.ts', 'tailwind.config.ts', 'postcss.config.js', 'commitlint.config.cjs'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+}
